ee1afce10bf0254afbc4dea1bb256f3ce15ef39f
KolaTofStudios <kolatof.studios@gmail.com>
2021-11-25 01:49:03 +0200

    k

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,79 @@ $(document).ready(function(){
   var t = $(this).attr("src");
   $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
   $("#myModal").modal();
+  console.log("otvori")
 });
 });
 
 
-//NAV BARA 
-(function($) { 
-  $(function() { 
 
-    //  OTVARQNE ZATVARQNE
-    $('#navbar-toggle').click(function() {
-      $('nav ul').slideToggle();
-    });
+$(".info").click(function(){
+  $(".backgrnd").css("visibility", "visible");
+  $(".zaglavie").css("visibility", "hidden");
+  $(".backgrnd").fadeIn(700);
+  $('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
+});
+
+$(".close").click(function(){
+  $(".backgrnd").fadeOut(500);
+  $(".zaglavie").css("visibility", "visible");
+  $('html, body').css({
+    overflow: 'auto',
+    height: 'auto'
+});
+});
 
 
-    // HAMBURGER MENU
-    $('#navbar-toggle').on('click', function() {
-      this.classList.toggle('active');
-    });
+//kolichestvo tochki
+const numStars = 1500;
 
-  }); 
-})(jQuery); 
+// namira random koordinati
+function getRandomPosition() {  
+  var y = $('body').width();;
+  var x = $('body').height();;
+    var randomX = Math.floor(Math.random()*x);
+    var randomY = Math.floor(Math.random()*y);
+    return [randomX,randomY];
+}
+
+//slaga tochkite
+for (let i = 0; i < numStars; i++) {
+  let star = document.createElement("div");  
+  star.className = "star";
+  var xy = getRandomPosition();
+    star.style.top = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+  document.body.append(star);
+}
+
+//smooth scroll function
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+function changeCur(){
+  document.body.style.cursor="url(../img/tie.cur), auto";
+}
+
+$(document).on('click', function(event){
+  changeCur()
+  // $('body').css('cursor', 'url(../img/tie.cur), auto');
+  console.log("click")
+});
+
+
+
